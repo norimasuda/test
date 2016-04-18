79f9ae7bbf2db68ae793f593e8499fcc117455bc
EC2 Default User <ec2-user@ip-172-31-21-214.us-west-2.compute.internal>
2016-04-18 07:25:03 +0000

    nmasuda

--- scripts/shipitfile.js
+++ scripts/shipitfile.js
@@ -10,7 +10,11 @@ module.exports = function (shipit) {
     staging: {
       servers: 'nodejs@ec2-52-11-19-57.us-west-2.compute.amazonaws.com',
       key: '/Users/Nori/Keypairs/myawskeypair3.pem'
-    }
+    },
+    staging-local: {
+      servers: 'nodejs@ec2-52-11-19-57.us-west-2.compute.amazonaws.com',
+      key: '/home/nodejs/keys/myaswkeypair3.pem'
+    },
   });
 
   shipit.task('pwd', function () {
@@ -20,6 +24,15 @@ module.exports = function (shipit) {
 
   shipit.task('task1', function () {
     return shipit.remote('cd galarm; echo TASK1 > task1.log; pwd');
+  });
+ 
+  shipit.blTask('local-deploy-clone', function() {
+    return shipit.local('sudo su - nodejs -c "cd /tmp; rm -fr test; git clone https://github.com/norimasuda/test.git"')
+  });
+
+  shipit.task('local-deploy', function() {
+    shipit.start('local-deploy-clone');
+    return shipit.local('sudo su - nodejs -c "cp /tmp/test/main.js /home/nodejs/app/test/main.js"')
   });
 };
 
