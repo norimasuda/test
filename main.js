c76e4085f268696b16042595d002522458cf7bda
Norihiko Masuda <nori.masuda@acintyo.com>
2016-04-18 11:36:26 +0900

    nmasuda: test

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var http = require("http");
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World!!!\n');
+    response.end('Hello World!\n');
 
     var d = new Date();
     console.log(d.toString() + ': Request from ' + request.connection.remoteAddress);
