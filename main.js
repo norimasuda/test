399de0d022cddda9ac02d49b27efd757a45f2b36
Norihiko Masuda <nori.masuda@acintyo.com>
2016-04-18 11:07:53 +0900

    nmasuda: add main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+var http = require("http");
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello World!!!\n');
+
+    var d = new Date();
+    console.log(d.toString() + ': Request from ' + request.connection.remoteAddress);
+}).listen(3000);
+
+console.log('Server running at port 3000');
+
